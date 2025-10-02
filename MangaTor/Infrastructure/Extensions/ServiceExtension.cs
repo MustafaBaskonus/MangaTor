710f381cb38e338cca2db36d66e51c1718bf4630
mustafa <bskns.mustafa@gmail.com>
2025-10-02 13:03:54 +0300

    Service Extension

---
+++ MangaTor/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,84 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Services.Contacts;
+using Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace MangaTor.Infrastructure.Extensions
+{
+    public static class ServiceExtension
+    {
+
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("MangaTor"));
+                options.EnableSensitiveDataLogging(true);
+            });
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+            }).AddEntityFrameworkStores<AppDbContext>();
+        }
+
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.Name = "MangaTor.Session";
+            });
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IComicService, ComicManager>();
+            services.AddScoped<IChapterService, ChapterManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IAuthService, AuthManager>();
+            services.AddScoped<ICommentService, CommentManager>();
+            services.AddScoped<IReactionService, ReactionManager>();
+            services.AddScoped<IRatingService, RatingManager>();
+            services.AddScoped<IProfileService, ProfileManager>();
+            services.AddScoped<IServiceManager, ServiceManager>();
+
+        }
+
+
+
+        public static void ConfigureApplicationCookie(this IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = new PathString("/Account/Login");
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.AccessDeniedPath = new PathString("/Account/AccessDenied");
+            });
+        }
+        public static void ConfigureRouting(this IServiceCollection services)
+        {
+            services.AddRouting(options =>
+            {
+                options.AppendTrailingSlash = false;
+                options.LowercaseUrls = true;
+            });
+        }
+
+    }
+}
--- MangaTor/Program.cs
+++ MangaTor/Program.cs
@@ -1,58 +1,18 @@
-using DAL.Context;
-using DAL.Entities;
 using MangaTor.Infrastructure.Extensions;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
-using Services;
-using Services.Contacts;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
-//Identity
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-    options.User.RequireUniqueEmail = true;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireDigit = false;
-    options.Password.RequiredLength = 6;
-}).AddEntityFrameworkStores<AppDbContext>();
 
-builder.Services.AddDbContext<AppDbContext>(options=>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("MangaTor"));
-});
-builder.Services.AddScoped<IComicService,ComicManager>();
-builder.Services.AddScoped<IChapterService,ChapterManager>();
-builder.Services.AddScoped<ICategoryService,CategoryManager>();
-builder.Services.AddScoped<IAuthService, AuthManager>();
-builder.Services.AddScoped<ICommentService, CommentManager>();
-builder.Services.AddScoped<IReactionService, ReactionManager>();
-builder.Services.AddScoped<IRatingService, RatingManager>();
-builder.Services.AddScoped<IProfileService, ProfileManager>();
-builder.Services.AddScoped<IServiceManager,ServiceManager>();
 
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options=>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.Name = "MangaTor.Session";
-});
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = new PathString("/Account/Login");
-    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
-    options.AccessDeniedPath = new PathString("/Account/AccessDenied");
-});
-
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureSession();
+builder.Services.ConfigureServiceRegistration();
+builder.Services.ConfigureRouting();//url lower 
+builder.Services.ConfigureApplicationCookie();
 
 
 builder.Services.AddAutoMapper(typeof(Program));
@@ -66,15 +26,22 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-//app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseSession();
+//app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapAreaControllerRoute(
+        name: "Admin",
+        areaName: "Admin",
+        pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+    );
     endpoints.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
